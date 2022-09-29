6b88dbc79aeecc5fcfead63a1c340aa92d3f6b0d
Chris <stylesuxx@gmail.com>
2022-09-29 23:19:38 +0200

    Pre-release repository (#19)
    * Add functionality to create repo for pre-release packages if available and newer then latest stable package. Resolves #17
    * Refactored functinoality to separate classes for better readbility
    * Add documentation about pre-release branch. Copy stable packages if a pre-release package is not available.

--- fetch-and-validate.py
+++ fetch-and-validate.py
@@ -1,10 +1,7 @@
 """Fetch ipk files from repositories and validate them."""
 import argparse
-import json
-import tarfile
-import urllib
-from github import Github
-from io import BytesIO
+from src.fetcher import Fetcher
+from src.html import HTML
 
 parser = argparse.ArgumentParser(
     description="Fetch ipk's from repositories and validate them")
@@ -14,9 +11,9 @@ parser.add_argument("--config", dest="config_file", metavar="CONFIG",
 parser.add_argument("--target", dest="ipk_target_dir", metavar="TARGET",
                     default="./build/packages",
                     help="Target dir for ipk downloads")
-parser.add_argument("--html", dest="html_output", metavar="HTML",
-                    default="./build/packages/index.html",
-                    help="Target file for the HTML index file")
+parser.add_argument("--html", dest="html_output_dir", metavar="HTML",
+                    default="./build/packages",
+                    help="Target dir for the HTML index file")
 parser.add_argument("--template", dest="html_template", metavar="TEMPLATE",
                     default="./index.html",
                     help="Template file for HTML listing")
@@ -26,111 +23,21 @@ args = parser.parse_args()
 token = args.token
 config_file = args.config_file
 ipk_target_dir = args.ipk_target_dir
-html_output = args.html_output
+html_output = "%s/index.html" % (args.html_output_dir)
 html_template = args.html_template
 
+html_output_prerelease = "%s-prerelease/index.html" % (args.html_output_dir)
 
-def build_html_rows(items):
-    """Build rows for the HTML display."""
-    rows = ""
-    for item in items:
-        rows += "<tr>"
-        rows += '<td><a href="%s">%s</a></td>' % (item["ipk"], item["Package"])
-        rows += '<td>%s</td>' % (item["Architecture"] or '')
-        rows += '<td>%s</td>' % (item["Version"] or '')
-        rows += '<td>%s</td>' % (item["Description"] or '')
-        rows += "</tr>"
+# Fetch assets for stable and pre-releases
+fetcher = Fetcher(token, config_file, ipk_target_dir)
+packages = fetcher.fetch()
 
-    return rows
+html = HTML(html_template)
 
+# Write package index
+stablePackages = sorted(packages['stable'], key=lambda d: d['Package'])
+html.write(html_output, stablePackages)
 
-def convert_to_dict(content):
-    """Convert the control content string to a dict."""
-    content_dict = {}
-
-    lines = content.splitlines()
-    for line in lines:
-        key_value = line.split(': ')
-        key = key_value[0]
-        value = None
-        if len(key_value) > 1:
-            value = key_value[1]
-
-        content_dict[key] = value
-
-    return content_dict
-
-
-def validate_ipk(path, names):
-    """Check if a given ipk has a valid control file."""
-    tar = tarfile.open(path)
-
-    members = tar.getmembers()
-    for member in members:
-        if member.name == "./control.tar.gz":
-            content = tar.extractfile(member).read()
-            control_tar = tarfile.open(fileobj=BytesIO(content))
-
-            control_members = control_tar.getmembers()
-            for control_member in control_members:
-                if control_member.name == "./control":
-                    content = control_tar.extractfile(control_member).read()
-                    content = content.decode("utf-8")
-
-                    content_dict = convert_to_dict(content)
-                    if content_dict['Package'].startswith(tuple(names)):
-                        return content_dict
-
-    return None
-
-
-def write_html_index(template_path, target_path, items):
-    """Generate and write the human readable index.html file."""
-    items = sorted(items, key=lambda d: d['Package'])
-
-    file = open(html_template)
-    html = file.read()
-    file.close()
-
-    rows = build_html_rows(items)
-    html = html.replace("{%ROWS%}", rows)
-
-    html_file = open(html_output, "w")
-    html_file.write(html)
-    html_file.close()
-
-
-github = Github(token)
-
-file = open(config_file)
-json = json.load(file)
-file.close()
-
-packages = []
-
-for repoEntry in json:
-    repo = repoEntry['repo']
-    names = repoEntry['names']
-
-    repository = github.get_repo(repo)
-    release = repository.get_latest_release()
-
-    assets = release.get_assets()
-    for asset in assets:
-        name = asset.name
-        url = asset.browser_download_url
-        if name.startswith(tuple(names)) and name.endswith(".ipk"):
-            print("Fetching: %s" % url)
-            targetPath = "%s/%s" % (ipk_target_dir, name)
-            urllib.request.urlretrieve(url, targetPath)
-
-            control = validate_ipk(targetPath, name)
-            if not control:
-                print("Invalid package: %s" % url)
-                print("Aborting...")
-                exit(1)
-
-            control["ipk"] = name
-            packages.append(control)
-
-write_html_index(html_template, html_output, packages)
+# write package index for pre-releases
+prereleasePackages = sorted(packages['prerelease'], key=lambda d: d['Package'])
+html.write(html_output_prerelease, prereleasePackages)
